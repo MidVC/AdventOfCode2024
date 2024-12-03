808f0b50ca2b4f344618d660e569b8c5d987baea
MidVC <lutony040720@gmail.com>
2024-12-02 23:36:18 -0500

    added template cpp file

---
+++ template.cpp
@@ -0,0 +1,17 @@
+/**
+ * Advent of Code 2024 - Day [X]
+ * Solution by: [Your Name]
+ * Repository: https://github.com/MidVC/AdventOfCode2024
+ */
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+    
+}
