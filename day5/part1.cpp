43f6952e934481fb0b818042f9a36032cae3395d
MidVC <lutony040720@gmail.com>
2024-12-25 01:03:18 -0500

    day5, modified template

---
+++ day5/part1.cpp
@@ -0,0 +1,44 @@
+/**
+ * Advent of Code 2024 - Day 5
+ * Solution by: MidC
+ * Repository: https://github.com/MidVC/AdventOfCode2024
+ */
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <sstream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    string line; char c; int value, value2, sum = 0;
+    bool map[100][100] = {0};
+
+    while (getline(cin, line)) {
+        if (line == "") break;
+        istringstream iss{line};
+        iss >> value >> c >> value2;
+        map[value][value2] = 1;
+        cout << value << ' ' << value2 << endl;
+    }
+
+    while (getline(cin, line)) {
+        vector<int> values;
+        istringstream iss{line};
+        while (iss >> value) {
+            values.push_back(value);
+            iss >> c;
+        }
+        bool flag = true;
+        for (int i = 0; i < values.size(); ++i) {
+            for (int j = i+1; j < values.size(); ++j) {
+                if (map[values[j]][values[i]]) flag = false;
+            }
+        }
+        if (flag) sum += values[values.size()/2];
+    }
+    cout << sum << endl;
+}
--- template.cpp
+++ template.cpp
@@ -1,6 +1,6 @@
 /**
  * Advent of Code 2024 - Day [X]
- * Solution by: [Your Name]
+ * Solution by: MidC
  * Repository: https://github.com/MidVC/AdventOfCode2024
  */
 
